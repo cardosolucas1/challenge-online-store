828b20cd34aa9065bc4bdd29799f46ca5ce5a4c5
Cardoso <lsc.cardosoo@gmail.com>
2020-06-10 12:40:19 -0300

    Updates

---
+++ precache-manifest.183f1a3246ad878bf06a833fcdad6e33.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e5f0f3f4e07d6afebe3c42547607c0",
+    "url": "/challenge-online-store/index.html"
+  },
+  {
+    "revision": "b3706b989906bc9d8c4c",
+    "url": "/challenge-online-store/static/css/main.a6ff1f3d.chunk.css"
+  },
+  {
+    "revision": "d601f8df3b9b3b08c417",
+    "url": "/challenge-online-store/static/js/2.78a873e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/challenge-online-store/static/js/2.78a873e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3706b989906bc9d8c4c",
+    "url": "/challenge-online-store/static/js/main.2fcae8bd.chunk.js"
+  },
+  {
+    "revision": "09718f6cc4e8eb8c6e18",
+    "url": "/challenge-online-store/static/js/runtime-main.c3213de5.js"
+  },
+  {
+    "revision": "f8a2d1e5c2c67dfbcc05573ca0cec648",
+    "url": "/challenge-online-store/static/media/arrow-down-icon.f8a2d1e5.svg"
+  },
+  {
+    "revision": "6c60b558af33aaaac6cdd825765b4d75",
+    "url": "/challenge-online-store/static/media/call-of-duty-infinite-warfare.6c60b558.png"
+  },
+  {
+    "revision": "cbc731609c8427658bb8f205fa552edd",
+    "url": "/challenge-online-store/static/media/call-of-duty-wwii.cbc73160.png"
+  },
+  {
+    "revision": "c519f8307ec5434a9ee5416960a6e65a",
+    "url": "/challenge-online-store/static/media/cart-icon.c519f830.svg"
+  },
+  {
+    "revision": "2e9e3fb313e8f385d497f01bb17a7dec",
+    "url": "/challenge-online-store/static/media/fifa-18.2e9e3fb3.png"
+  },
+  {
+    "revision": "d9ecc34886456a1d89e3e02fcadc0966",
+    "url": "/challenge-online-store/static/media/horizon-zero-dawn.d9ecc348.png"
+  },
+  {
+    "revision": "358b12304faeadd1d2f4af68a844c138",
+    "url": "/challenge-online-store/static/media/mortal-kombat-xl.358b1230.png"
+  },
+  {
+    "revision": "c23a7ce9c73d8e7171e4f175f4468483",
+    "url": "/challenge-online-store/static/media/shards-of-darkness.c23a7ce9.png"
+  },
+  {
+    "revision": "4ad04c13c312115d676223bef55a8532",
+    "url": "/challenge-online-store/static/media/super-mario-odyssey.4ad04c13.png"
+  },
+  {
+    "revision": "aecd10c4f358fe4e95f871ebab211b21",
+    "url": "/challenge-online-store/static/media/terra-media-sombras-de-mordor.aecd10c4.png"
+  },
+  {
+    "revision": "1d707379e0dad1386d67eb941a5fd2ad",
+    "url": "/challenge-online-store/static/media/the-witcher-iii-wild-hunt.1d707379.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a27763c947d68853ae275cda16948ccf.js"
+  "/challenge-online-store/precache-manifest.183f1a3246ad878bf06a833fcdad6e33.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/challenge-online-store/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
